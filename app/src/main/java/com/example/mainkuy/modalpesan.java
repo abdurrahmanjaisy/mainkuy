52e57a37b3ca4fb36078fa5dc1e1a08052bb9f8b
Jais <abdurrahmanjaisy@ugm.mail.com>
2019-11-02 00:15:38 +0700

    Terbaruh

---
+++ app/src/main/java/com/example/mainkuy/modalpesan.java
@@ -0,0 +1,14 @@
+package com.example.mainkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modalpesan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modalpesan);
+    }
+}

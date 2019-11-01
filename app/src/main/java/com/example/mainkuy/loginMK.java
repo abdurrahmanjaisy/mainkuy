c8231de9eaeddcabca3c771ef1da6c9504a1da26
Jais <abdurrahmanjaisy@ugm.mail.com>
2019-11-01 15:30:01 +0700

    first commit

---
+++ app/src/main/java/com/example/mainkuy/loginMK.java
@@ -0,0 +1,14 @@
+package com.example.mainkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginMK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_mk2);
+    }
+}

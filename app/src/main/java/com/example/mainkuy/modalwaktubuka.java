72aa90ba1166507000dd76b5687d210cef0975a6
Jais <abdurrahmanjaisy@ugm.mail.com>
2019-11-02 15:02:06 +0700

    syuda

---
+++ app/src/main/java/com/example/mainkuy/modalwaktubuka.java
@@ -0,0 +1,14 @@
+package com.example.mainkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modalwaktubuka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modalwaktubuka);
+    }
+}

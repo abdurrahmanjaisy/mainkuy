8bd1a26d5d584974ce837d5afc2abce54067bff9
Jais <abdurrahmanjaisy@ugm.mail.com>
2019-11-03 21:01:26 +0700

    finish

---
+++ app/src/main/java/com/example/mainkuy/waktubayar.java
@@ -0,0 +1,14 @@
+package com.example.mainkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class waktubayar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waktubayar);
+    }
+}
